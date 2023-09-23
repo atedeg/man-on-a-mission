7647c5917f2e67a5540b7f97feb257c5fc244aa9
ndido98 <ndido98@gmail.com>
2023-09-23 15:05:38 +0200

    chore: add main skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+// Clippy stuff
+#![forbid(unsafe_code)]
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+#![allow(clippy::cargo_common_metadata)]
+#![warn(clippy::allow_attributes_without_reason)]
+#![warn(clippy::disallowed_script_idents)]
+#![warn(clippy::empty_structs_with_brackets)]
+#![warn(clippy::float_cmp_const)]
+#![warn(clippy::integer_division)]
+#![warn(clippy::string_slice)]
+#![warn(clippy::string_to_string)]
+#![warn(clippy::try_err)]
+#![warn(clippy::unnecessary_self_imports)]
+#![warn(clippy::print_stderr)]
+#![warn(clippy::print_stdout)]
+
+use std::time::Duration;
+
+use anyhow::Context;
+use sqlx::postgres::PgPoolOptions;
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    // Load .env file, if present
+    dotenvy::dotenv().ok();
+
+    // Initialize logging
+    tracing_subscriber::fmt::init();
+
+    let db_connection_string = dotenvy::var("DATABASE_URL")
+        .context("DATABASE_URL must be set")
+        .unwrap();
+    info!("connecting to database: {}", db_connection_string);
+    let db_pool = PgPoolOptions::new()
+        .max_connections(4)
+        .acquire_timeout(Duration::from_secs(30))
+        .connect(&db_connection_string)
+        .await
+        .context("Could not connect to database")
+        .unwrap();
+
+    info!("running migrations");
+    sqlx::migrate!()
+        .run(&db_pool)
+        .await
+        .context("Could not run migrations")
+        .unwrap();
+
+    todo!()
 }
